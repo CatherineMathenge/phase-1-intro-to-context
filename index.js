b6a090e999dc706e99ea244e4c369c373b090cd9
Catherine <catherine.mathenge@strathmore.edu>
2023-07-07 14:29:13 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,167 @@
-// Your code here
+// Function to create an employee record
+function createEmployeeRecord(data) {
+    return {
+      firstName: data[0],
+      familyName: data[1],
+      title: data[2],
+      payPerHour: data[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  // Function to create employee records from CSV data
+  function createEmployeeRecords(data) {
+    return data.map(createEmployeeRecord);
+  }
+  
+  // Function to add a timeIn event to an employee's record
+  function createTimeInEvent(employee, dateTime) {
+    const [date, hour] = dateTime.split(" ");
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+    return employee;
+  }
+  
+  // Function to add a timeOut event to an employee's record
+  function createTimeOutEvent(employee, dateTime) {
+    const [date, hour] = dateTime.split(" ");
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      date: date,
+      hour: parseInt(hour, 10)
+    });
+    return employee;
+  }
+  
+  // Function to calculate hours worked on a specific date for an employee
+  function hoursWorkedOnDate(employee, date) {
+    const timeInEvent = employee.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employee.timeOutEvents.find(event => event.date === date);
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+  }
+  
+  // Function to calculate wages earned on a specific date for an employee
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+  // Function to calculate all wages for an employee
+  function allWagesFor(employee) {
+    const datesWorked = employee.timeInEvents.map(event => event.date);
+    const wages = datesWorked.map(date => wagesEarnedOnDate(employee, date));
+    return wages.reduce((total, wage) => total + wage, 0);
+  }
+  
+  // Function to calculate payroll for multiple employees
+  function calculatePayroll(employees) {
+    const wages = employees.map(employee => allWagesFor(employee));
+    return wages.reduce((total, wage) => total + wage, 0);
+  }
+  
+  // Test cases using the "describe" and "it" functions
+  describe("The payroll system", function () {
+    // Test cases for createEmployeeRecord
+    describe("populates a record from an Array", function () {
+      it("has a function called createEmployeeRecord", function () {
+        expect(createEmployeeRecord).to.exist;
+      });
+  
+      describe("createEmployeeRecord", function () {
+        // Test cases for createEmployeeRecord function
+      });
+    });
+  
+    // Test cases for createEmployeeRecords
+    describe("process an Array of Arrays into an Array of employee records", function () {
+      it("has a function called createEmployeeRecords", function () {
+        expect(createEmployeeRecords).to.exist;
+      });
+  
+      describe("createEmployeeRecords", function () {
+        // Test cases for createEmployeeRecords function
+      });
+    });
+  
+    // Test cases for createTimeInEvent
+    describe("it adds a timeIn event Object to an employee's record of timeInEvents when provided an employee record and Date/Time String and returns the updated record", function () {
+      it("has a function called createTimeInEvent", function () {
+        expect(createTimeInEvent).to.exist;
+      });
+  
+      describe("createTimeInEvent", function () {
+        // Test cases for createTimeInEvent function
+      });
+    });
+  
+    // Test cases for createTimeOutEvent
+    describe("it adds a timeOut event Object to an employee's record of timeOutEvents when provided an employee record and Date/Time String and returns the updated record", function () {
+      it("has a function called createTimeOutEvent", function () {
+        expect(createTimeOutEvent).to.exist;
+      });
+  
+      describe("createTimeOutEvent", function () {
+        // Test cases for createTimeOutEvent function
+      });
+    });
+  
+    // Test cases for hoursWorkedOnDate
+    describe("Given an employee record with a date-matched timeInEvent and timeOutEvent", function () {
+      it("hoursWorkedOnDate calculates the hours worked when given an employee record and a date", function () {
+        expect(hoursWorkedOnDate).to.exist;
+      });
+  
+      describe("hoursWorkedOnDate", function () {
+        // Test cases for hoursWorkedOnDate function
+      });
+    });
+  
+    // Test cases for wagesEarnedOnDate
+    describe("Given an employee record with a date-matched timeInEvent and timeOutEvent", function () {
+      it("wagesEarnedOnDate multiplies the hours worked by the employee's rate per hour", function () {
+        expect(wagesEarnedOnDate).to.exist;
+      });
+  
+      describe("wagesEarnedOnDate", function () {
+        // Test cases for wagesEarnedOnDate function
+      });
+    });
+  
+    // Test cases for allWagesFor
+    describe("Given an employee record with MULTIPLE date-matched timeInEvent and timeOutEvent", function () {
+      it("allWagesFor aggregates all the dates' wages and adds them together", function () {
+        expect(allWagesFor).to.exist;
+      });
+  
+      describe("allWagesFor", function () {
+        // Test cases for allWagesFor function
+      });
+    });
+  
+    // Test cases for calculatePayroll
+    describe("Given an array of multiple employees", function () {
+      it("calculatePayroll aggregates all the dates' wages and adds them together", function () {
+        expect(calculatePayroll).to.exist;
+      });
+  
+      describe("calculatePayroll", function () {
+        // Test cases for calculatePayroll function
+      });
+    });
+  
+    // Test cases for the provided CSV data
+    describe("runs payroll using the mock data provided by Ultron data systems", function () {
+      describe("Dependent functions: createEmployeeRecords", function () {
+        // Test cases for createEmployeeRecords function
+      });
+  
+      describe("Full Payroll Test", function () {
+        // Test cases for calculatePayroll function
+      });
+    });
+  });
+  
